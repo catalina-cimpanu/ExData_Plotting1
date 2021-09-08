9a28a358c4a07633b2d4bec9b2c4edb33477d78e
Catalina <catalina.cimpanu@gmail.com>
2021-09-08 22:02:38 +0200

    created start file, a file with the common trunk for all files

---
+++ start.R
@@ -0,0 +1,26 @@
+# install.packages(c("gdata", "data.table", "dplyr"))
+
+library(data.table)
+library(dplyr)
+library(gdata) 
+library(hms)
+
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "dataset.zip")
+unzip(zipfile = "dataset.zip", exdir = ".")
+
+# object size
+object.size(fread("household_power_consumption.txt"))
+
+# in megabytes -> humanReadable from gdata package
+# https://stackoverflow.com/questions/29787452/how-do-i-quickly-convert-the-size-element-of-file-info-from-bytes-to-kb-mb-g
+humanReadable(object.size(fread("household_power_consumption.txt")))
+
+consumption <- fread(file = "household_power_consumption.txt", na.strings="?")
+selected_consumption <- subset(consumption, consumption$Date == "1/2/2007" | consumption$Date == "2/2/2007")
+
+selected_consumption$Date <- as.Date(selected_consumption$Date, format = "%d/%m/%Y")
+selected_consumption$Time <- as.hms(selected_consumption$Time)
+
+# https://stackoverflow.com/questions/12034424/convert-hourminutesecond-hhmmss-string-to-proper-time-class
+
+
