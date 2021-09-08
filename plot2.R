807c8cd964f50a75bd302c53f46359b015574d5a
Catalina <catalina.cimpanu@gmail.com>
2021-09-08 22:58:41 +0200

    improved code plot 2

--- plot2.R
+++ plot2.R
@@ -1,16 +1,19 @@
 # run common trunk of the code
 source("start.R")
 
-# create DateTime variable (strptime didn't save as POSIXct, so i used as.POSIXct)
+# create DateTime variable (with strptimei couldn't save as POSIXct directly in the dataframe, so i used as.POSIXct)
 selected_consumption$DateTime <- as.POSIXct(paste(selected_consumption$Date, selected_consumption$Time))
 
 # open png Graphics File Device
 png("plot2.png",  width = 480, height = 480)
 
 # create plot
-
-plot(selected_consumption$DateTime, selected_consumption$Global_active_power, type = "l", pch = NA, lwd = 1, xlab = "", 
-     ylab = "Global Active Power (kilowatts)", ylim = range(selected_consumption$Global_active_power))
+plot(selected_consumption$DateTime, selected_consumption$Global_active_power, 
+     type = "l", 
+     xlab = "", 
+     ylab = "Global Active Power (kilowatts)", 
+     ylim = range(selected_consumption$Global_active_power)
+     )
 
 # close Graphics File Device
 dev.off()
